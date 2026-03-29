5c556d131d69db537815ad495dd6d85b4d3753da
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-30 02:32:48 +0500

    create vpc

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,1 +1,15 @@
 
+output "vpc_id" {
+  description = "The ID of our VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "The IP range of our VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "availability_zones" {
+  description = "AZs available in our region"
+  value       = data.aws_availability_zones.available.names
+}

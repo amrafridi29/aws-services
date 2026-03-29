4c90b03b04dbe0b5747c4fddf916b182efface7a
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-30 01:20:08 +0500

    feat(iam):wip

--- iam/main.tf
+++ iam/main.tf
@@ -22,7 +22,38 @@ resource "aws_iam_policy" "s3_policy" {
 }
 
 resource "aws_iam_user_policy_attachment" "developer_s3" {
-    user = aws_iam_user.developer.name
-    policy_arn = aws_iam_policy.s3_policy.arn
-  
+  user       = aws_iam_user.developer.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+# The Role itself (with trust policy)
+resource "aws_iam_role" "app_role" {
+  name = "my-app-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+# Attach AWS managed policy to the role
+resource "aws_iam_role_policy_attachment" "app_role_s3" {
+  role       = aws_iam_role.app_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+# Instance profile (wraps the role so EC2 can use it)
+resource "aws_iam_instance_profile" "app_profile" {
+  name = "my-app-instance-profile"
+  role = aws_iam_role.app_role.name
 }

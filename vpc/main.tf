8a97044e68fe48dd7aacc50f1e1b6c4ae43ae5f7
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-31 18:14:30 +0500

    add security groups (alb, app, db)

--- vpc/main.tf
+++ vpc/main.tf
@@ -109,3 +109,90 @@ resource "aws_route" "private_nat" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.main[count.index].id
 }
+
+resource "aws_security_group" "alb" {
+  name        = "${var.project_name}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    "Name" = "${var.project_name}-alb-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+  security_group_id = aws_security_group.alb.id
+  from_port         = 80
+  to_port           = 80
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  description       = "Allow HTTP traffic from internet"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_https" {
+  security_group_id = aws_security_group.alb.id
+  from_port         = 443
+  to_port           = 443
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  description       = "Allow HTTPS traffic from internet"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_all_out" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic"
+}
+
+resource "aws_security_group" "app" {
+  name        = "${var.project_name}-app-sg"
+  description = "Security group for application servers"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    "Name" = "${var.project_name}-app-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_from_alb" {
+  security_group_id            = aws_security_group.app.id
+  referenced_security_group_id = aws_security_group.alb.id
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  description                  = "Allow traffic from ALB to app servers"
+}
+
+resource "aws_vpc_security_group_egress_rule" "app_all_out" {
+  security_group_id = aws_security_group.app.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic from app servers"
+}
+
+resource "aws_security_group" "db" {
+  name        = "${var.project_name}-db-sg"
+  description = "Security group for database servers"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-db-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db_from_app" {
+  security_group_id            = aws_security_group.db.id
+  referenced_security_group_id = aws_security_group.app.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+  description                  = "Allow traffic from app servers to database"
+}
+
+resource "aws_vpc_security_group_egress_rule" "db_all_out" {
+  security_group_id = aws_security_group.db.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic from database servers"
+}
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -47,3 +47,18 @@ output "nat_gateway_public_ips" {
   description = "Public Elastic IPs of all NAT Gateways"
   value       = aws_eip.nat[*].public_ip
 }
+
+output "alb_security_group_id" {
+  description = "Security Group ID for the load balancer"
+  value       = aws_security_group.alb.id
+}
+
+output "app_security_group_id" {
+  description = "Security Group ID for app servers"
+  value       = aws_security_group.app.id
+}
+
+output "db_security_group_id" {
+  description = "Security Group ID for databases"
+  value       = aws_security_group.db.id
+}

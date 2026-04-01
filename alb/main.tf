eff19e8c04c8f3f9ec85392fc5c20ebdaa650008
Muhammad Amir <amrafridi.29@gmail.com>
2026-04-02 01:57:28 +0500

    wip on alb

--- alb/main.tf
+++ alb/main.tf
@@ -260,7 +260,7 @@ resource "aws_instance" "app" {
   EOF
 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 30
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
@@ -313,5 +313,76 @@ resource "aws_ebs_snapshot" "app_data_backup" {
 #   tags = {
 #     Name      = "${var.project_name}-restored-volume"
 #     ManagedBy = "Terraform"
+#   }
+# }
+
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public[*].id
+  security_groups    = [aws_security_group.alb.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.project_name}-lb"
+  }
+}
+
+resource "aws_alb_target_group" "app" {
+  name     = "${var.project_name}-app-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app" {
+  target_group_arn = aws_alb_target_group.app.arn
+  target_id        = aws_instance.app.id
+  port             = 8080
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.app.arn
+  }
+}
+
+# Route /api/* to the API target group
+# resource "aws_lb_listener_rule" "api" {
+#   listener_arn = aws_lb_listener.http.arn
+#   priority     = 100
+
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.api.arn
+#   }
+
+#   condition {
+#     path_pattern {
+#       values = ["/api/*"]
+#     }
 #   }
 # }
--- alb/outputs.tf
+++ alb/outputs.tf
@@ -97,3 +97,18 @@ output "snapshot_id" {
   description = "ID of the data volume snapshot"
   value       = aws_ebs_snapshot.app_data_backup.id
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.main.arn
+}
+
+output "target_group_arn" {
+  description = "ARN of the app target group"
+  value       = aws_alb_target_group.app.arn
+}
